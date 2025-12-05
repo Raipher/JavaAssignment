6cadff459c5ba99e0933ce6530fe2d305e6acf80
User <User@host.docker.internal>
2025-12-05 18:41:20 +0800

    commit 1

---
+++ src/main/java/com/mycompany/javaassignment/JavaAssignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaassignment;
+
+/**
+ *
+ * @author User
+ */
+public class JavaAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
